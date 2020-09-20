db94ddf7f30b1853fab8e1b1ac8cb9c4e480a6bf
03771929610 <rogercomp@gmail.com>
2020-09-20 08:58:39 -0300

    Adicionando front-web

--- backend/src/main/java/com/devsuperior/dspesquisa/entities/Game.java
+++ backend/src/main/java/com/devsuperior/dspesquisa/entities/Game.java
@@ -14,7 +14,6 @@ import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import com.devsuperior.dspesquisa.entities.enums.Platform;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name="tb_game")
